7425cfbcaadeae5cfba8405c66b360d5dbf431a4
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2025-01-30 14:04:59 +0000

    Updating paths

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     // plugins: [react()],
-    base: '/ts-recap/',
+
+    base: '/nology-jan-25-ts-recap/',
 });
